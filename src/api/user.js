de807b1e9ba9d738db2d73b605596c3586da80ee
guosurui <13012615613@163.com>
2023-07-16 04:45:41 +0800

    随便绘制一些用户页的信息

---
+++ src/api/user.js
@@ -0,0 +1,14 @@
+
+
+export function getUserInfo() {
+    let json = {
+        "nickName": "狗蛋",
+        "sex": 1,
+        "quote": "我是狗蛋",
+        "avatarUrl": "https://wx.qlogo.cn/mmopen/vi_32/Q0j4TwGT",
+        "city": "广州",
+        "automobile": "奔驰",
+        "age": 18,
+    }
+    return json
+    }
--- src/pages/user/index.jsx
+++ src/pages/user/index.jsx
@@ -1,47 +1,103 @@
-import { Component } from 'react'
-import { connect } from 'react-redux'
-import { View, Button, Text } from '@tarojs/components'
+import { Component } from "react";
+import { connect } from "react-redux";
+import { View, Button, Text } from "@tarojs/components";
+import {
+  Cell,
+  CellGroup,
+  Toast,
+  NavBar,
+  MiniNavBar,
+  Icon,
+  Grid,
+  GridItem,
+  Tag,
+  Image,
+} from "@antmjs/vantui";
+import { Collapse, CollapseItem } from "@antmjs/vantui";
+import { add, minus, asyncAdd } from "../../actions/counter";
+import { getUserInfo } from "../../api/user";
+import "./index.scss";
 
-import { add, minus, asyncAdd } from '../../actions/counter'
-import './index.scss'
-
-
-@connect(({ counter }) => ({
-  counter
-}), (dispatch) => ({
-  add () {
-    dispatch(add())
-  },
-  dec () {
-    dispatch(minus())
-  },
-  asyncAdd () {
-    dispatch(asyncAdd())
-  }
-}))
 class User extends Component {
-  componentWillReceiveProps (nextProps) {
-    console.log(this.props, nextProps)
+  componentWillReceiveProps(nextProps) {
+    console.log(this.props, nextProps);
   }
 
-  componentWillUnmount () { }
+  componentWillUnmount() {}
 
-  componentDidShow () { }
+  componentDidShow() {}
 
-  componentDidHide () { }
+  componentDidHide() {}
 
-  render () {
+  render() {
     return (
-      <View className='user'>
-        <Button className='add_btn' onClick={this.props.add}>+</Button>
-        <Button className='dec_btn' onClick={this.props.dec}>-</Button>
-        <Button className='dec_btn' onClick={this.props.asyncAdd}>async</Button>
-        <View><Text>{this.props.counter.num}</Text></View>
-        <View><Text>Hello, World</Text></View>
+      <View>
+        <NavBar title='用户' safeAreaInsetTop={false} />
+        {/* <CellGroup inset>
+        <Cell title="单元格" value="内容" />
+        <Cell title="单元格" value="内容" label="描述信息" />
+      </CellGroup> */}
+        <View>
+          <Cell
+            center
+            value='内容'
+            isLink
+            renderTitle={
+              <View
+                style={{ display: "flex", alignItems: "center", margin: 12 }}
+              >
+                <Image
+                  round
+                  width={96}
+                  height={96}
+                  src='https://img.yzcdn.cn/vant/cat.jpeg'
+                />
+                <Text>{getUserInfo().nickName}</Text>
+              </View>
+            }
+          />
+        </View>
+        <View>
+          <Collapse accordion>
+            <CollapseItem title='鲁A12345' name='1'>
+              <View>
+                <Cell title='车牌号' value='鲁A12345' />
+                <Cell title='车辆类型' value='小型汽车' />
+                <Cell title='车辆品牌' value='大众' />
+                <Cell title='车辆型号' value='大众' />
+                <Cell title='车辆颜色' value='白色' />
+              </View>
+            </CollapseItem>
+            <CollapseItem title='鲁A23456' name='2'>
+              <View>
+                <Cell title='车牌号' value='鲁A23456' />
+                <Cell title='车辆类型' value='小型汽车' />
+                <Cell title='车辆品牌' value='大众' />
+                <Cell title='车辆型号' value='大众' />
+                <Cell title='车辆颜色' value='白色' />
+              </View>
+            </CollapseItem>
+            <CollapseItem title='鲁A34567' name='3'>
+              <View>
+                <Cell title='车牌号' value='鲁A34567' />
+                <Cell title='车辆类型' value='小型汽车' />
+                <Cell title='车辆品牌' value='大众' />
+                <Cell title='车辆型号' value='大众' />
+                <Cell title='车辆颜色' value='白色' />
+              </View>
+            </CollapseItem>
+          </Collapse>
+        </View>
+        <Grid clickable columnNum={4} border={false} gutter={0} iconSize='32px'>
+          <GridItem icon='chat-o' text='文字' />
+          <GridItem icon='photo-o' text='图片' />
+          <GridItem icon='camera-o' text='拍摄' />
+          <GridItem icon='photo-o' text='图片' />
+          <GridItem icon='photo-o' text='图片' />
+        </Grid>
       </View>
-    )
+    );
   }
 }
 
-export default User
-
+export default User;
